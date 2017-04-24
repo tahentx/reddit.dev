ef5e6b5faa1cad59bc449274c242f75f29b94381
Todd Hendricks <hendricks.ta@gmail.com>
2017-04-24 13:33:48 -0500

    dice roll game

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,24 +11,39 @@
 |
 */
 
-Route::get('/uppercase/{word}', function ($word) {
-    	return strtoupper('$word');
-});
+// Route::get('/uppercase/{word}', function ($word) {
+//     	return strtoupper('$word');
+// });
 
-Route::get('/increment/{number}', function ($number) {
-    if (is_numeric($number)){
-    return $number + 1;
-		else {
-		return $number . "this can' be incremented"		
-		}
+// Route::get('/increment/{number}', function ($number) {
+//     if (is_numeric($number)){
+//     return $number + 1;
+// 		else {
+// 		return $number . "this can' be incremented";	
+// 		}
+// 	}
+// });
+
+// Route::get('/add/{num1}/{num2}', function ($num1, $num2){
+// 	if (is_numeric($num1) && (is_numeric($num2)){
+// 		return $num1 + $num2;
+// 	} else{
+// 		return "both parameters must be numeric";
+// 	}    
+// });
+
+// Route::get('/sayhello/{name}', function($name)
+// {
+// 	$data = ['name' => $name];
+//     return view('my-first-view', $data);
+// });
+
+Route::get('/rolldice/{number}', function($number){
+	$randomNumber =  random_int(1, 6);
+	return view('roll-dice',['number'=>$number, 'randomNumber'=>$randomNumber]);
+	if ($randomNumber === $number) {
+		return "Great guess!";
+	} else{
+		return "Keep trying";
 	}
 });
-
-Route::get('/add/{num1}/{num2}', function ($num1, $num2){
-	if (is_numeric($num1) && (is_numeric($num2)){
-		return $num1 + $num2;
-	} else{
-		return "both parameters must be numeric";
-	}    
-});
-
---
+++ resources/views/roll-dice.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h3>Your guess was...</h3>
+<?php echo $number ?>
+<br>
+<h3>The dice rolled....</h3>
+<?php echo $randomNumber  ?>
+<br>
+</body>
+</html>
